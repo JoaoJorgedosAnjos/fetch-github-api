e697f2e026ea2a5bb0054895a7ad57807f44aba5
JoaoJorge <joaojorgedosanjos@gmail.com>
2022-11-07 21:47:36 -0300

    update js code

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,4 +1,4 @@
-import { baseUrl , commitsQuantatity  } from "/src/scripts/variables.js"
+import { baseUrl , commitsQuantatity  } from "../variables.js"
 
 async function getEvents(userName){
     const response = await fetch(`${baseUrl}/${userName}/events?per_page=${commitsQuantatity}`)
